cbe40e631d5a00aab1d748b5393413692ca3685b
extremehaf <lucasdemelolima@gmail.com>
2018-08-25 03:13:30 -0300

    Projeto Inicial

---
+++ cameracrop/src/test/java/com/android/camera/crop/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.android.camera.crop;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <crop_activity href="http://d.android.com/tools/testing">Testing documentation</crop_activity>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
